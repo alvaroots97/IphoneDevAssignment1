deb9217d26f34e4c7ddeb15a52798f83800f4d1d
Alvaro Alva <7mesino.97@gmail.com>
2020-09-09 18:24:36 -0400

    Initial Commit

---
+++ IphoneDevAssignment1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IphoneDevAssignment1
+//
+//  Created by Alvaro Alva on 9/9/20.
+//  Copyright © 2020 Alvaro Alva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
